2cd32d4b0d02631cf0ee8807fa0e4dc734c122dc
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-04-16 16:30:01 +0100

    Defined a race-free reduction and added the transitive closure of reduction.

--- src/XFuture.v
+++ src/XFuture.v
@@ -30,21 +30,30 @@ Module Races.
     r_ah: access_history
   }.
 
-  Inductive Reduces: state -> state -> Prop :=
+  Inductive Reduces: state -> Lang.effect -> state -> Prop :=
   | reduces_def:
     forall s s' cg sh o cg' sh',
     Lang.Reduces s o s' ->
     CG.Reduces cg o cg' ->
     Shadow_Ext.Reduces cg sh o sh' ->
-    Reduces {| r_state:=s; r_cg:=cg; r_ah:=sh|} {|r_state:=s';r_cg:=cg'; r_ah:=sh'|}.
+    Reduces {| r_state:=s; r_cg:=cg; r_ah:=sh|} o {|r_state:=s';r_cg:=cg'; r_ah:=sh'|}.
 
-  Inductive RF_Reduces: state -> state -> Prop :=
-  | rf_reduces_def:
-    forall s s',
-    RaceFree (r_ah s) (r_cg s) ->
-    RaceFree (r_ah s') (r_cg s') ->
-    Reduces s s' ->
-    RF_Reduces s s'.
-    
+  Inductive RaceFreeReduces: state -> Lang.effect -> state -> Prop :=
+  | race_free_reduces_def:
+    forall s s' cg sh o cg' sh',
+    Lang.Reduces s o s' ->
+    CG.Reduces cg o cg' ->
+    Shadow_Ext.Reduces cg sh o sh' ->
+    RaceFreeReduces {| r_state:=s; r_cg:=cg; r_ah:=sh|} o {|r_state:=s';r_cg:=cg'; r_ah:=sh'|}.
+
+  Inductive ReducesTrace: state -> list Lang.effect -> state -> Prop :=
+  | reduces_trace_nil:
+    forall s,
+    ReducesTrace s nil s
+  | reduces_trace_cons:
+    forall s s' s'' e t,
+    ReducesTrace s t s' ->
+    RaceFreeReduces s' e s'' ->
+    ReducesTrace s (e::t) s''.
 
 End Races.
