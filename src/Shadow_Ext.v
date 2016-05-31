a5cffdef4a5bb0434c86c2a24d2e6f0cba11b339
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-05-31 10:04:15 -0500

    Cleaned up and removed dead code.

--- src/Shadow_Ext.v
+++ src/Shadow_Ext.v
@@ -20,12 +20,25 @@ Require Lang.
 Require CG.
 
 Module Trace.
+
+  (** There are two kinds of events we observe: reading and writing to shared memory. *)
+
   Inductive mode := READ | WRITE.
+
+  (**
+    An represents the kind of access the node in the CG responsible for the access
+    and the target variable.
+   *)
+
   Structure access := {
-    a_t : mode;
-    a_src : CG.Node.node;
-    a_dst : mid
+    a_t : mode;           (* how *)
+    a_src : CG.Node.node; (* when *)
+    a_dst : mid           (* what *)
   }.
+
+  (**
+    Converts an event from the language to an event we can interpret.
+    *)
 
   Definition e_to_a cg (e:Lang.effect) :=
   match e with
@@ -47,7 +60,12 @@ End Trace.
 Section Shadow_Ext.
   Import Trace.
 
+  (** An access history is just a sequence of optional accesses; none when 
+    the event is neither a read nor a write. *)
+
   Definition access_history := list (option access).
+
+  (** compares if two access modes. *)
 
   Definition mode_eq m1 m2 :=
   match (m1, m2) with
@@ -56,6 +74,8 @@ Section Shadow_Ext.
   | _ => false
   end.
 
+  (** Matches what and how *)
+
   Definition a_match x m (a:option access) :=
   match a with
   | Some a =>
@@ -63,13 +83,21 @@ Section Shadow_Ext.
   | None => false
   end.
 
+  (** Returns any access that matches what and how. *)
+
   Definition ah_filter (x:mid) (m:mode) (ah:access_history) := filter (a_match x m) ah.
 
+  (** Returns all reads on a certain variable. *)
+
   Definition ah_reads (x:mid) (ah:access_history) := ah_filter x READ ah.
+
+  (** Returns all writes on a certain variable *)
 
   Definition ah_writes x ah := ah_filter x READ ah.
 
   Section Defs.
+
+  (** Checks if a language event is either a read or a write. *)
 
   Definition is_access (o:Lang.op) :=
   match o with
@@ -78,83 +106,26 @@ Section Shadow_Ext.
   | _ => false
   end.
 
-  Inductive AccessPre cg : Lang.effect -> Prop :=
-  | access_pre_true:
-    forall x o,
-    MT.In x (CG.cg_nodes cg) ->
-    is_access o = true ->
-    AccessPre cg (x, o)
-  | access_pre_false:
-    forall x o,
-    is_access o = false ->
-    AccessPre cg (x, o).
-
-  Inductive RaceFreeAccess cg : option access -> option access -> Prop :=
-  | race_free_access_lhs_none:
-    forall a,
-    RaceFreeAccess cg a None
-  | race_free_access_rhs_none:
-    forall a,
-    RaceFreeAccess cg None a
-  | race_free_access_mismatch:
-    forall a b,
-    a_dst a <> a_dst b ->
-    RaceFreeAccess cg (Some a) (Some b)
-  | race_free_access_read_read:
-    forall a b,
-    a_dst a = a_dst b ->
-    a_t a = READ ->
-    a_t b = READ ->
-    RaceFreeAccess cg (Some a) (Some b)
-  | race_free_access_lhs_write:
-    forall a b,
-    a_dst a = a_dst b ->
-    a_t a = WRITE ->
-    CG.Ordered cg (a_src a) (a_src b) ->
-    RaceFreeAccess cg (Some a) (Some b)
-  | race_free_access_rhs_write:
-    forall a b,
-    a_dst a = a_dst b ->
-    a_t b = WRITE ->
-    CG.Ordered cg (a_src a) (a_src b) ->
-    RaceFreeAccess cg (Some a) (Some b).
-
-  Definition RaceFreeCons cg ah a  := Forall (RaceFreeAccess cg a) ah.
-
   Definition ah_add cg e ah := e_to_a cg e::ah.
 
-  Inductive AccessHistory: list CG.computation_graph -> list Lang.effect -> access_history -> Prop :=
-  | access_history_nil:
-    forall cg,
-    AccessHistory (cg::nil) nil nil
-  | access_of_cons:
-    forall t (ah:access_history) l cg e,
-    AccessHistory l t ah ->
-    AccessPre cg e ->
-    AccessHistory (cg::l) (e::t) (ah_add cg e ah).
+  (** Two accesses are racy *)
 
-  Inductive RaceFreeAccessHistory: list CG.computation_graph -> list Lang.effect -> access_history -> Prop :=
-  | race_free_access_history_nil:
-    forall cg,
-    RaceFreeAccessHistory (cg::nil) nil nil
-  | race_free_access_of_cons:
-    forall t (ah:access_history) l cg e,
-    RaceFreeAccessHistory l t ah ->
-    AccessPre cg e ->
-    RaceFreeCons cg ah (e_to_a cg e) ->
-    RaceFreeAccessHistory (cg::l) (e::t) (e_to_a cg e::ah).
+  Inductive RacyAccess cg : option access -> option access -> Prop :=
+  | racy_access_def:
+    forall a b,
+    a_src a <> a_src b ->
+    a_dst a = a_dst b ->
+    (a_t a = WRITE \/ a_t b = WRITE) ->
+    CG.MHP cg (a_src a) (a_src b) ->
+    RacyAccess cg (Some a) (Some b).
 
-  Inductive RaceFreeReduces cg ah e: access_history -> Prop :=
-  | race_free_reduces_def:
-    AccessPre cg e ->
-    RaceFreeCons cg ah (e_to_a cg e) ->
-    RaceFreeReduces cg ah e (e_to_a cg e::ah).
+  (** Two accesses are race-free. *)
 
-  Inductive Reduces cg ah e: access_history -> Prop :=
-  | reduces_write:
-    AccessPre cg e ->
-    Reduces cg ah e (ah_add cg e ah).
+  Definition RaceFreeAccess cg a b := ~ RacyAccess cg a b.
 
+  (** An access can be appended to the access-history, yielding a race-free history. *)
+
+  Definition RaceFreeCons cg ah a  := Forall (RaceFreeAccess cg a) ah.
 
   Variable cg:CG.computation_graph.
 
@@ -180,22 +151,18 @@ Section Shadow_Ext.
     CG.MHP cg n1 n2 ->
     HasRace h.
 
-
-  Inductive OrderedAccesses h: Prop :=
-  | safe_reads_def:
-    (forall x y,
-      Reads x h ->
+  Inductive RaceFreeRef h: Prop :=
+  | race_free_ref_def:
+    (forall x a y,
+      Access a x h ->
       Writes y h ->
-      CG.Ordered cg x y) ->
-    (forall x y,
-      Writes x h ->
-      Writes y h ->
-      CG.Ordered cg x y) ->
-    OrderedAccesses h.
+      x <> y ->
+      CG.Comparable cg x y) ->
+    RaceFreeRef h.
 
   Inductive RaceFree : Prop :=
   | race_free_def:
-    (forall h, OrderedAccesses h) ->
+    (forall h, RaceFreeRef h) ->
     RaceFree.
 
   Inductive Racy : Prop :=
@@ -205,4 +172,113 @@ Section Shadow_Ext.
       Racy.
   End Defs.
 
+  Let race_free_inv_1:
+    forall cg x y t1 t2 h,
+    RaceFreeAccess cg
+      (Some {| a_t := t1; a_src := x; a_dst := h |})
+      (Some {| a_t := t2; a_src := y; a_dst := h |}) ->
+    x <> y ->
+    (t1 = WRITE \/ t2 = WRITE) ->
+    CG.Comparable cg x y.
+  Proof.
+    unfold RaceFreeAccess.
+    intros.
+    destruct (CG.hb_dec cg x y).
+    - auto using CG.comparable_left_right.
+    - auto using CG.comparable_right_left.
+    - contradiction.
+    - contradiction H.
+      auto using racy_access_def.
+  Qed.
+
+  Let race_free_ref_to_race_free_access:
+    forall n1 n2 t1 t2 h cg ah,
+    RaceFreeRef cg ah h ->
+    In (Some {| a_t := t1; a_src := n1; a_dst := h |}) ah ->
+    In (Some {| a_t := t2; a_src := n2; a_dst := h |}) ah ->
+    RaceFreeAccess cg
+      (Some {| a_t := t1; a_src := n1; a_dst := h |})
+      (Some {| a_t := t2; a_src := n2; a_dst := h |}).
+  Proof.
+    intros.
+    inversion H.
+    unfold RaceFreeAccess.
+    unfold not; intros.
+    inversion H3; simpl in *; subst; clear H7.
+    destruct H8; subst; assert (CG.Comparable cg n1 n2) by eauto using CG.comparable_symm;
+    apply CG.comparable_to_not_mhp in H4; contradiction.
+  Qed.
+
+  Let race_free_access_ref_neq:
+    forall t1 t2 n1 n2 h1 h2 cg,
+    h1 <> h2 ->
+    RaceFreeAccess cg
+      (Some {| a_t := t1; a_src := n1; a_dst := h1 |})
+      (Some {| a_t := t2; a_src := n2; a_dst := h2 |}).
+  Proof.
+    intros.
+    unfold RaceFreeAccess; intuition.
+    inversion H0; simpl in *; subst.
+    contradiction H; auto.
+  Qed.
+
+  Let race_free_access_none:
+    forall a1 a2 cg,
+    (a1 = None \/ a2 = None) ->
+    RaceFreeAccess cg a1 a2.
+  Proof.
+    unfold RaceFreeAccess.
+    intuition; inversion H0; subst.
+    - inversion H5.
+    - inversion H6.
+  Qed.
+
+  Let race_free_to_race_free_access:
+    forall ah cg a1 a2,
+    RaceFree cg ah ->
+    In a1 ah ->
+    In a2 ah ->
+    RaceFreeAccess cg a1 a2.
+  Proof.
+    intros.
+    inversion H.
+    destruct a1, a2; auto.
+    destruct a, a0.
+    destruct (MID.eq_dec a_dst0 a_dst1); rewrite mid_eq_rw in *; auto.
+    subst.
+    eauto.
+  Qed.
+
+  Lemma race_free_cons:
+    forall cg ah a,
+    RaceFree cg ah ->
+    RaceFreeCons cg ah a ->
+    RaceFree cg (a::ah).
+  Proof.
+    intros.
+    apply race_free_def.
+    intros.
+    apply race_free_ref_def.
+    intros.
+    unfold RaceFreeCons in *.
+    destruct H1.
+    - subst.
+      inversion H2; clear H2.
+      + inversion H1; subst; clear H1.
+        contradiction H3; auto.
+      + rewrite Forall_forall in *.
+        apply H0 in H1.
+        apply race_free_inv_1 in H1; auto.
+    - inversion H2; clear H2.
+      + subst.
+        rewrite Forall_forall in *.
+        apply H0 in H1.
+        apply race_free_inv_1 in H1; auto using CG.comparable_symm.
+      + assert (X: RaceFreeAccess cg
+          (Some {| a_t := a0; a_src := x; a_dst := h |})
+          (Some {| a_t := WRITE; a_src := y; a_dst := h |})) by eauto.
+        apply race_free_inv_1 in X; auto.
+  Qed.
+
 End Shadow_Ext.
+
