44d9155a597a7c2bb32ccdcb52fd330b642a4f6d
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-06-27 11:06:42 -0500

    Rewriting shadow memory to add support for values.

--- src/Memory.v
+++ src/Memory.v
@@ -1,119 +1,138 @@
+Set Implicit Arguments.
+
+Require Import Coq.Lists.List.
 Require Import Tid.
 Require Import Lang.
-Require Import Dep.
 Require Import Mid.
+Require Import Shadow.
+Require Import Node.
+
+Module Locals.
+Section Defs.
+  Variable A:Type.
+
+  Inductive op :=
+  | CONS : A -> node -> op
+  | NEW : list A -> op.
+
+  Definition task_local := list A.
+
+  Definition local_memory := MN.t task_local.
+
+  Inductive Reduces (m:local_memory): (node * op) -> local_memory -> Prop :=
+  | reduces_add:
+    forall l x n n',
+    MN.MapsTo n l m ->
+    ~ MN.In n' m ->
+    Reduces m (n', CONS x n) (MN.add n' (x::l) m)
+  | reduces_new:
+    forall l n,
+    ~ MN.In n m ->
+    Reduces m (n, NEW l) (MN.add n l m).
+
+  Inductive MapsTo (n:node) (x:A) (ls:local_memory) : Prop :=
+  | local_def:
+    forall l,
+    MN.MapsTo n l ls ->
+    List.In x l ->
+    MapsTo n x ls.
+
+End Defs.
+End Locals.
 
 Section Defs.
-  Definition local := list (tid * dep). (*MT.t (list dep).*)
-  Definition global := MM.t (option dep).
 
-  Definition memory := (global * local) % type.
+  Import Locals.
+
+  Inductive datum :=
+  | d_task : tid -> datum
+  | d_mem : mid -> datum
+  | d_any : datum.
+
+
+  Definition global_memory := access_history datum.
+
+  Definition memory := (global_memory * (local_memory datum)) % type.
 
   Definition m_global (m:memory) := fst m.
 
   Definition m_local (m:memory) := snd m.
 
   Inductive op :=
-(*  | LOCAL_REM: dep -> op*)
-  | GLOBAL_CREATE: mid -> option dep -> op
-  | GLOBAL_PUT: mid -> option dep -> op
-  | GLOBAL_GET: mid -> op
-  | FUTURE: tid -> list dep -> op
-  | FORCE: tid -> list dep -> op.
-(*
-  Definition local_update f (t:tid) (l:local) : local :=
-  match MT.find t l with
-  | Some ds => MT.add t (f ds)%list l
-  | _ => l
-  end.
-*)
-  Notation local_entry := (tid * dep) % type.
+  | CONTINUE: op
+  | GLOBAL_ALLOC: mid -> datum -> op
+  | GLOBAL_WRITE: mid -> datum -> op
+  | GLOBAL_READ: mid -> op
+  | FUTURE: node -> list datum -> op
+  | FORCE: node -> datum -> op.
 
-  Definition local_add t d (l:local) : local := cons (t,d) l. (*local_update (cons d) % list t.*)
+  Definition effect := (node * node * op) % type.
 
-  Fixpoint local_add_all t ds (l:local) : local :=
-  match ds with
-  | cons d ds => local_add t d (local_add_all t ds l)
-  | nil => l
-  end.
+  Require Import CG.
 
-  Let local_entry_eq_dec :
-    forall (x y : local_entry),
-    { x = y } + { x <> y }.
-  Proof.
-    intros.
-    destruct x as (x1,x2), y as (y1, y2).
-    destruct (tid_eq_dec x1 y1), (dep_eq_dec x2 y2);
-    subst;
-    auto;
-    right;
-    intuition;
-    inversion H;
-    subst;
-    contradiction n;
-    auto.
-  Qed.
-
-  Definition local_remove (t:tid) (d:dep) (l:local) := List.remove local_entry_eq_dec (t,d) l. (*local_update (List.remove dep_eq_dec d) t.*)
-
-(*  Definition local_create (t:tid) (ds:list dep) (l:local) : local := MT.add t ds l.*)
-
-  Definition effect := (tid * op) % type.
-
-  Inductive In (t:tid) (l:local) : Prop :=
-  | in_def:
-    forall d,
-    List.In (t, d) l ->
-    In t l.
-
-  Inductive Local (l:local) x (d:dep) : Prop :=
-  | local_def:
-    List.In (x,d) l ->
-    Local l x d.
+  Variable cg: computation_graph.
 
   Inductive Reduces : memory -> effect -> memory -> Prop :=
-  | reduces_g_get: (* read *)
-    forall g l x t d,
-    Local l t (d_mid x) ->
-    MM.MapsTo x (Some d) g ->
-    Reduces (g, l) (t, GLOBAL_GET x) (g, local_add t d l)
-  | reduces_g_put: (* write *)
-    forall g l x t o,
-    Local l t (d_mid x) ->
-    Reduces (g, l) (t, GLOBAL_PUT x o) (MM.add x o g, l)
-(*  | reduces_l_rem: (* local store of a constant *)
-    forall l g t o,
-    In t l ->
-    Reduces (g, l) (t, LOCAL_REM o) (g, local_remove t o l)*)
-  | reduces_g_create: (* malloc *)
-    forall g l x t o,
-    ~ MM.In x g ->
-    Local l t (d_mid x) ->
-    Reduces (g, l) (t, GLOBAL_CREATE x o) (MM.add x o g, l)
-  | reduces_future: (* future *)
-    forall t t' ds l g,
-    List.Forall (Local l t) ds ->
-    ~ In t' l ->
-    Reduces (g, l) (t, FUTURE t' ds) (g, local_add_all t' ds (local_add t (d_tid t') l))
-  | reduce_force: (* force *)
-    forall g l t t' d ds,
-    List.Forall (Local l t) ds ->
-    Reduces (g, l) (t, FORCE t' ds) (g, local_add t d l).
+  | reduces_g_read:
+    forall g l d n n' x l' g',
+    Locals.MapsTo n (d_mem x) l ->
+    Shadow.MapsTo x d g cg ->
+    Locals.Reduces l (n', CONS d n) l' ->
+    Shadow.Reduces cg g (x, {| a_when := n; a_what:=@READ datum |}) g' ->
+    Reduces (g, l) (x, GLOBAL_READ x) (g', l')
+
+  | reduces_g_write:
+    forall g (l:local_memory datum) x d n n' g',
+    Locals.MapsTo n d l ->
+    Locals.MapsTo n (d_mem x) l ->
+    Shadow.Reduces cg g (x, {| a_when:=n; a_what:=WRITE d|}) g' ->
+    Reduces (g, l) (n, n', GLOBAL_WRITE x d) (g', l)
+
+  | reduces_g_alloc:
+    forall g l x n n' d l' g',
+    Locals.MapsTo n d l ->
+    Shadow.Reduces cg g (x, {|a_when:=n;a_what:=ALLOC d|}) g' ->
+    Locals.Reduces l (n', CONS (d_mem x) n) l' ->
+    Reduces (g, l) (n, n', GLOBAL_ALLOC x d) (g', l')
+
+  | reduces_future:
+    forall nx nx' ny ds l g y l' l'',
+    List.Forall (fun d => Locals.MapsTo nx d l) ds ->
+    Node.MapsTo y ny (fst cg) ->
+    Locals.Reduces l (nx', CONS (d_task y) nx) l' ->
+    Locals.Reduces l' (ny, NEW ds) l'' ->
+    Reduces (g, l) (nx, nx', FUTURE ny ds) (g, l)
+
+  | reduce_force:
+    forall g l nx nx' ny d l',
+    Locals.MapsTo ny d l ->
+    Locals.Reduces l (nx, CONS d nx') l' ->
+    Reduces (g, l) (nx, nx', FORCE ny d) (g, l').
+
+
+  Inductive Knows (l:local_memory datum) : tid * tid -> Prop :=
+  | knows_def:
+    forall n x y,
+    Node.MapsTo x n (fst cg) ->
+    Locals.MapsTo n (d_task y) l ->
+    Knows l (x, y).
 
 End Defs.
 
-Module Lang.
 
-  Definition to_lang_op (o:op) : Lang.op :=
-  match o with
-(*  | LOCAL_REM _ => Lang.TAU*)
-  | GLOBAL_CREATE x _ => Lang.WRITE x
-  | GLOBAL_PUT x _ => Lang.WRITE x
-  | GLOBAL_GET x => Lang.READ x
-  | FUTURE x _ => Lang.FUTURE x
-  | FORCE x _ => Lang.FORCE x
-  end.
+Section SR.
+  Require SJ_CG.
 
-  Definition to_lang_effects : list effect -> list Lang.effect := List.map (fun (e:effect) => (fst e, to_lang_op (snd e))).
+  Definition LocalToKnows (l:Locals.local_memory datum) cg sj :=
+    forall l p,
+    Knows cg l p ->
+    SJ_CG.Knows (fst cg) sj p.
 
-End Lang.
+  Lemma local_to_knows_reduces:
+    forall a t cg k,
+    Events.Reduces k e k' ->
+    CG.Reduces cg e cg' ->
+    SJ cg k sj ->
+    
+
+End SR.
--- src/Shadow.v
+++ src/Shadow.v
@@ -5,7 +5,7 @@ Require Import HJ.Mid.
 Require Import HJ.Cid.
 Require Import HJ.Var.
 Require Import HJ.Dep.
-(*Require Import Node.*)
+Require Import Node.
 
 (* ----- end of boiler-plate code ---- *)
 
@@ -19,264 +19,143 @@ Require Import Coq.Structures.OrderedTypeEx.
 
 Require CG.
 
-Module Trace.
-
+Section Defs.
   (** There are two kinds of events we observe: reading and writing to shared memory. *)
 
-  Inductive mode := READ | WRITE.
+  Variable A:Type.
 
   (**
     An represents the kind of access the node in the CG responsible for the access
     and the target variable.
    *)
 
+  Inductive op := READ | WRITE : A -> op | ALLOC : A -> op.
+
   Structure access := {
-    a_t : mode;           (* how *)
-    a_src : nat; (* when *)
-    a_dst : mid           (* what *)
+    a_when : node;
+    a_what : op
   }.
 
-  Definition access_history := list access.
+Notation WR n x := {| a_when := n; a_what := WRITE x|}.
+Notation RD n := {| a_when := n; a_what := READ|}.
+Notation AL n x := {| a_when := n; a_what := ALLOC x|}.
 
-End Trace.
+  Inductive IsRead a : Prop :=
+  | read_def:
+    a_what a = READ ->
+    IsRead a.
 
-Section Shadow_Ext.
-  Import Trace.
+  Inductive Write a (x:A) : Prop :=
+  | write_write:
+    a_what a = WRITE x ->
+    Write a x
+  | write_alloc:
+    a_what a = ALLOC x ->
+    Write a x.
 
-  (** An access history is just a sequence of optional accesses; none when 
-    the event is neither a read nor a write. *)
+  Inductive IsWrite a : Prop :=
+  | write_def:
+    forall x,
+    Write a x ->
+    IsWrite a.
 
-  (** compares if two access modes. *)
-
-  Definition mode_eq m1 m2 :=
-  match (m1, m2) with
-  | (READ, READ) => true
-  | (WRITE, WRITE) => true
-  | _ => false
-  end.
-
-  (** Matches what and how *)
-
-  Definition a_match x m (a:access) :=
-  andb (Mid.eqb x (a_dst a)) (mode_eq m (a_t a)).
-
-  (** Returns any access that matches what and how. *)
-
-  Definition ah_filter (x:mid) (m:mode) (ah:access_history) := filter (a_match x m) ah.
-
-  (** Returns all reads on a certain variable. *)
-
-  Definition ah_reads (x:mid) (ah:access_history) := ah_filter x READ ah.
-
-  (** Returns all writes on a certain variable *)
-
-  Definition ah_writes x ah := ah_filter x READ ah.
-
-  Section Defs.
-
-  Inductive LastWrite cg ah (a:access) (m:mid) : Prop :=
-  | last_write_def:
-    a_t a = WRITE ->
-    a_dst a = m ->
-    (forall a', a_t a' = WRITE -> List.In (Some a') ah -> CG.HB cg (a_src a') (a_src a)) ->
-    LastWrite cg ah a m.
+  Definition access_history := MM.t (list access).
 
   (** Two accesses are racy *)
 
   Inductive RacyAccess cg : access -> access -> Prop :=
   | racy_access_def:
     forall a b,
-    a_src a <> a_src b ->
-    a_dst a = a_dst b ->
-    (a_t a = WRITE \/ a_t b = WRITE) ->
-    CG.MHP cg (a_src a) (a_src b) ->
+    a_when a <> a_when b ->
+    (IsWrite a \/ IsWrite b) ->
+    CG.MHP cg (a_when a) (a_when b) ->
     RacyAccess cg a b.
-
-  (** Two accesses are race-free. *)
 
   Definition RaceFreeAccess cg a b := ~ RacyAccess cg a b.
 
-  (** An access can be appended to the access-history, yielding a race-free history. *)
-
   Definition RaceFreeCons cg ah a  := Forall (RaceFreeAccess cg a) ah.
 
-  Variable cg:CG.computation_graph.
+Section History.
+
+  Require Import CG.
+
+  Inductive Reduces (cg:computation_graph) ah: (mid * access) -> access_history -> Prop :=
+  | reduces_write:
+    forall n x y l,
+    MM.MapsTo x l ah ->
+    Reduces cg ah (x, WR n y) (MM.add x (WR n y::l) ah)
+  | reduces_read:
+    forall n x l,
+    MM.MapsTo x l ah ->
+    Reduces cg ah (x, {|a_when:=n;a_what:=READ|}) (MM.add x (RD n::l) ah)
+  | reduces_alloc:
+    forall x n y,
+    ~ MM.In x ah ->
+    Reduces cg ah (x, AL n y) (MM.add x (AL n y::nil) ah).
+
+  Section Access.
+
+    Variable h:mid.
+
+    Inductive Access (a:access) (ah:access_history) : Prop :=
+    | access_def:
+      forall l,
+      MM.MapsTo h l ah ->
+      List.In a l ->
+      Access a ah.
+
+    Let Le cg (a:access) b := a = b \/ CG.HB cg (a_when a) (a_when b).
+
+    Inductive LastWrite (n:node) (x:A) ah cg : Prop :=
+    | last_write_def:
+      forall (a:access),
+      Access a ah ->
+      a_when a = n ->
+      Write a x ->
+      (forall b, Access b ah -> IsWrite b -> Le cg b a) ->
+      LastWrite n x ah cg.
+
+    Inductive MapsTo (x:A) ah cg : Prop :=
+    | maps_to_def:
+      forall n,
+      LastWrite n x ah cg ->
+      MapsTo x ah cg.
+
+    Inductive HasRace ah cg : Prop :=
+    | has_race_def:
+      forall a b,
+      Access a ah ->
+      Access b ah ->
+      (IsWrite a \/ IsWrite b) ->
+      CG.MHP cg (a_when a) (a_when b) ->
+      HasRace ah cg.
+
+    Inductive RaceFreeRef ah cg: Prop :=
+    | race_free_ref_def:
+      (forall x y,
+        Access x ah ->
+        Access y ah ->
+        IsWrite y ->
+        x <> y ->
+        CG.Comparable cg (a_when x) (a_when y)) ->
+      RaceFreeRef ah cg.
+  End Access.
 
   Variable ah:access_history.
 
-  Definition Access a t h := List.In {| a_t:=a; a_src:=t; a_dst:=h|} ah. 
-
-  Definition Writes t h := Access WRITE t h.
-
-  Definition Reads t h := Access READ t h.
-
-  Inductive CoAccess (x y:nat) (h:mid): Prop :=
-  | co_access_def:
-    forall a,
-    Writes x h ->
-    Access a y h ->
-    CoAccess x y h.
-
-  Inductive HasRace h : Prop :=
-  | has_race_def:
-    forall n1 n2,
-    CoAccess n1 n2 h ->
-    CG.MHP cg n1 n2 ->
-    HasRace h.
-
-  Inductive RaceFreeRef h: Prop :=
-  | race_free_ref_def:
-    (forall x a y,
-      Access a x h ->
-      Writes y h ->
-      x <> y ->
-      CG.Comparable cg x y) ->
-    RaceFreeRef h.
+  Variable cg:CG.computation_graph.
 
   Inductive RaceFree : Prop :=
   | race_free_def:
-    (forall h, RaceFreeRef h) ->
+    (forall h, RaceFreeRef h ah cg) ->
     RaceFree.
 
   Inductive Racy : Prop :=
     racy_def:
       forall h,
-      HasRace h ->
+      HasRace h ah cg ->
       Racy.
-  End Defs.
+  End History.
 
-  Let race_free_inv_1:
-    forall cg x y t1 t2 h,
-    RaceFreeAccess cg
-      {| a_t := t1; a_src := x; a_dst := h |}
-      {| a_t := t2; a_src := y; a_dst := h |} ->
-    x <> y ->
-    (t1 = WRITE \/ t2 = WRITE) ->
-    CG.Comparable cg x y.
-  Proof.
-    unfold RaceFreeAccess.
-    intros.
-    destruct (CG.hb_dec cg x y).
-    - auto using CG.comparable_left_right.
-    - auto using CG.comparable_right_left.
-    - contradiction.
-    - contradiction H.
-      auto using racy_access_def.
-  Qed.
+End Defs.
 
-  Let race_free_ref_to_race_free_access:
-    forall n1 n2 t1 t2 h cg ah,
-    RaceFreeRef cg ah h ->
-    In {| a_t := t1; a_src := n1; a_dst := h |} ah ->
-    In {| a_t := t2; a_src := n2; a_dst := h |} ah ->
-    RaceFreeAccess cg
-      {| a_t := t1; a_src := n1; a_dst := h |}
-      {| a_t := t2; a_src := n2; a_dst := h |}.
-  Proof.
-    intros.
-    inversion H.
-    unfold RaceFreeAccess.
-    unfold not; intros.
-    inversion H3; simpl in *; subst; clear H5.
-    destruct H6; subst; assert (CG.Comparable cg n1 n2) by eauto using CG.comparable_symm;
-    apply CG.comparable_to_not_mhp in H5; contradiction.
-  Qed.
-
-  Let race_free_access_ref_neq:
-    forall t1 t2 n1 n2 h1 h2 cg,
-    h1 <> h2 ->
-    RaceFreeAccess cg
-       {| a_t := t1; a_src := n1; a_dst := h1 |}
-       {| a_t := t2; a_src := n2; a_dst := h2 |}.
-  Proof.
-    intros.
-    unfold RaceFreeAccess; intuition.
-    inversion H0; simpl in *; subst.
-    contradiction H; auto.
-  Qed.
-
-  Let race_free_to_race_free_access:
-    forall ah cg a1 a2,
-    RaceFree cg ah ->
-    In a1 ah ->
-    In a2 ah ->
-    RaceFreeAccess cg a1 a2.
-  Proof.
-    intros.
-    inversion H.
-    destruct a1, a2; auto.
-    destruct (MID.eq_dec a_dst0 a_dst1); rewrite mid_eq_rw in *; auto.
-    subst.
-    eauto.
-  Qed.
-
-  Lemma race_free_cons:
-    forall cg ah a,
-    RaceFree cg ah ->
-    RaceFreeCons cg ah a ->
-    RaceFree cg (a::ah).
-  Proof.
-    intros.
-    apply race_free_def.
-    intros.
-    apply race_free_ref_def.
-    intros.
-    unfold RaceFreeCons in *.
-    destruct H1.
-    - subst.
-      inversion H2; clear H2.
-      + inversion H1; subst; clear H1.
-        contradiction H3; auto.
-      + rewrite Forall_forall in *.
-        apply H0 in H1.
-        apply race_free_inv_1 in H1; auto.
-    - inversion H2; clear H2.
-      + subst.
-        rewrite Forall_forall in *.
-        apply H0 in H1.
-        apply race_free_inv_1 in H1; auto using CG.comparable_symm.
-      + assert (X: RaceFreeAccess cg
-           {| a_t := a0; a_src := x; a_dst := h |}
-           {| a_t := WRITE; a_src := y; a_dst := h |}) by eauto.
-        apply race_free_inv_1 in X; auto.
-  Qed.
-
-End Shadow_Ext.
-(*
-Module Lang.
-  Import Trace.
-
-  (** Checks if a language event is either a read or a write. *)
-
-  Definition is_access (o:Lang.op) :=
-  match o with
-  | Lang.WRITE _ => true
-  | Lang.READ _ => true
-  | _ => false
-  end.
-
-  (**
-    Converts an event from the language to an event we can interpret.
-    *)
-
-  Definition e_to_a cg (e:Lang.effect) : option access :=
-  match e with
-  | (t, Lang.WRITE m) =>
-    match CG.cg_lookup t cg with
-    | Some n => Some {| a_t:= WRITE; a_src:=n; a_dst:= m |}
-    | _ => None
-    end
-  | (t, Lang.READ m) =>
-    match CG.cg_lookup t cg with
-    | Some n => Some {| a_t:= READ; a_src:=n; a_dst:= m |}
-    | _ => None
-    end
-  | _ => None
-  end.
-
-  Definition effects_to_ah_0 cg : list Lang.effect -> access_history := List.omap (e_to_a cg).
-
-  Definition ah_add cg e ah := e_to_a cg e::ah.
-End Lang.
-
-*)
