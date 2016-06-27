38f15a70a215fdde9991dcb8306c17328b000364
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-06-27 12:23:59 -0500

    Rewrote the shared memory module.

--- src/Memory.v
+++ src/Memory.v
@@ -63,10 +63,10 @@ Section Defs.
   | GLOBAL_ALLOC: mid -> datum -> op
   | GLOBAL_WRITE: mid -> datum -> op
   | GLOBAL_READ: mid -> op
-  | FUTURE: node -> list datum -> op
-  | FORCE: node -> datum -> op.
+  | FUTURE: tid -> list datum -> op
+  | FORCE: tid -> datum -> op.
 
-  Definition effect := (node * node * op) % type.
+  Definition effect := (tid * op) % type.
 
   Require Import CG.
 
@@ -74,40 +74,63 @@ Section Defs.
 
   Inductive Reduces : memory -> effect -> memory -> Prop :=
   | reduces_g_read:
-    forall g l d n n' x l' g',
-    Locals.MapsTo n (d_mem x) l ->
-    Shadow.MapsTo x d g cg ->
+    forall g l d n n' (x:tid) y l' g',
+    (* a global read is a continue in the CG *)
+    ReductionResult (x, CG.CONTINUE) cg (R_CONTINUE (C (n,n'))) ->
+    (* the reference y is in the locals of task x *)
+    Locals.MapsTo n (d_mem y) l ->
+    (* the contents of reference y is datum d *)
+    Shadow.MapsTo y d g cg ->
+    (* add datum d to the locals of x *)
     Locals.Reduces l (n', CONS d n) l' ->
-    Shadow.Reduces cg g (x, {| a_when := n; a_what:=@READ datum |}) g' ->
-    Reduces (g, l) (x, GLOBAL_READ x) (g', l')
+    (* update the shared memory to record the read *)
+    Shadow.Reduces cg g (y, {| a_when := n'; a_what:=@READ datum |}) g' ->
+    Reduces (g, l) (x, GLOBAL_READ y) (g', l')
 
   | reduces_g_write:
-    forall g (l:local_memory datum) x d n n' g',
+    forall g (l:local_memory datum) (x:tid) (y:mid) d n n' g',
+    (* a global write is a continue in the CG *)
+    ReductionResult (x, CG.CONTINUE) cg (R_CONTINUE (C (n,n'))) ->
+    (* datum d being written is in the locals of task x *)
     Locals.MapsTo n d l ->
-    Locals.MapsTo n (d_mem x) l ->
-    Shadow.Reduces cg g (x, {| a_when:=n; a_what:=WRITE d|}) g' ->
-    Reduces (g, l) (n, n', GLOBAL_WRITE x d) (g', l)
+    (* the target reference is also in the locals of task x *)
+    Locals.MapsTo n (d_mem y) l ->
+    (* update the shared memory to record the write of datum d at reference y *)
+    Shadow.Reduces cg g (y, {| a_when:=n'; a_what:=WRITE d|}) g' ->
+    Reduces (g, l) (x, GLOBAL_WRITE y d) (g', l)
 
   | reduces_g_alloc:
-    forall g l x n n' d l' g',
+    forall g l (x:tid) n n' d l' g' y,
+    (* a global alloc is a continue edge in the CG *)
+    ReductionResult (x, CG.CONTINUE) cg (R_CONTINUE (C (n,n'))) ->
+    (* the datum being alloc'ed is a local *)
     Locals.MapsTo n d l ->
-    Shadow.Reduces cg g (x, {|a_when:=n;a_what:=ALLOC d|}) g' ->
-    Locals.Reduces l (n', CONS (d_mem x) n) l' ->
-    Reduces (g, l) (n, n', GLOBAL_ALLOC x d) (g', l')
+    (* update the shared memory with an alloc *)
+    Shadow.Reduces cg g (y, {|a_when:=n;a_what:=ALLOC d|}) g' ->
+    (* add reference y to the locals of task x *)
+    Locals.Reduces l (n', CONS (d_mem y) n) l' ->
+    Reduces (g, l) (x, GLOBAL_ALLOC y d) (g', l')
 
   | reduces_future:
-    forall nx nx' ny ds l g y l' l'',
+    forall x nx nx' ny ds l g y l' l'',
+    ReductionResult (x, CG.FORK y) cg (R_FORK (F (nx,ny)) (C (nx,nx'))) ->
+    (* the locals of the new task are copied from the locals of the current task *)
     List.Forall (fun d => Locals.MapsTo nx d l) ds ->
-    Node.MapsTo y ny (fst cg) ->
+    (* add task y to the locals of x *)
     Locals.Reduces l (nx', CONS (d_task y) nx) l' ->
+    (* set the locals of y to be ds *)
     Locals.Reduces l' (ny, NEW ds) l'' ->
-    Reduces (g, l) (nx, nx', FUTURE ny ds) (g, l)
+    Reduces (g, l) (x, FUTURE y ds) (g, l)
 
   | reduce_force:
-    forall g l nx nx' ny d l',
+    forall g l nx nx' ny d l' x y,
+    (* CG reduced with a join *)
+    ReductionResult (x, CG.JOIN y) cg (R_JOIN (J (ny,nx')) (C (nx,nx'))) ->
+    (* Datum d is in the locals of y *)
     Locals.MapsTo ny d l ->
+    (* Add d to the locals of x *)
     Locals.Reduces l (nx, CONS d nx') l' ->
-    Reduces (g, l) (nx, nx', FORCE ny d) (g, l').
+    Reduces (g, l) (x, FORCE y d) (g, l').
 
 
   Inductive Knows (l:local_memory datum) : tid * tid -> Prop :=
@@ -118,21 +141,3 @@ Section Defs.
     Knows l (x, y).
 
 End Defs.
-
-
-Section SR.
-  Require SJ_CG.
-
-  Definition LocalToKnows (l:Locals.local_memory datum) cg sj :=
-    forall l p,
-    Knows cg l p ->
-    SJ_CG.Knows (fst cg) sj p.
-
-  Lemma local_to_knows_reduces:
-    forall a t cg k,
-    Events.Reduces k e k' ->
-    CG.Reduces cg e cg' ->
-    SJ cg k sj ->
-    
-
-End SR.
