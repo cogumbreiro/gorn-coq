5d4df54d77f1c72a03f1c030c269463e3c28c4cf
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-09-28 23:53:00 -0400

    Rewrote initialization script.

---
+++ configure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version=8.5 # `opam show --field=version coq`
+
+check_pkg() {
+    opam search $@ > /dev/null
+}
+
+install_coq() {
+  echo "Installing Coq over OPAM..."
+  opam repo add coq-released https://coq.inria.fr/opam/released &&
+  (opam install coq || true)
+}
+
+install_aniceto() {
+  echo "Installing Aniceto..." &&
+  opam pin add --dev-repo coq-aniceto https://bitbucket.org/cogumbreiro/aniceto-coq.git
+}
+coq_shell_url="https://raw.githubusercontent.com/gares/opam-coq-shell/master/src/opam-coq"
+(check_pkg coq-shell || install_coq) &&
+(check_pkg coq-aniceto || install_aniceto) &&
+(test -f Makefile || coq_makefile -f _CoqProject -o Makefile)
+
