d411e65f331dae5b30d81f217a8dbb98aa2932f7
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-09-28 22:54:19 -0400

    Improved visualization.

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -1,9 +1,9 @@
 module Html = Dom_html
 
-open List;;
-open Printf;;
-open String;;
-open Stringext;;
+open List
+open Printf
+open String
+open Stringext
 
 let rec int_of_nat x =
     match x with
@@ -51,9 +51,37 @@ let event_of_string s =
     ) with
     | _ -> None  
 
+let filter_duplicates lst =
+  let rec is_member n mlst =
+    match mlst with
+    | [] -> false
+    | h::tl ->
+        begin
+          if h=n then true
+          else is_member n tl
+        end
+  in
+  let rec loop lbuf =
+    match lbuf with
+    | [] -> []
+    | h::tl ->
+        begin
+        let rbuf = loop tl
+        in
+          if is_member h rbuf then rbuf
+          else h::rbuf
+        end
+  in
+  loop lst
+
 let string_of_event e =
-    Printf.sprintf "(%d, %s, %d)"
-    (int_of_nat (Sj.op_src e)) (string_of_event_t (Sj.op_t e)) (int_of_nat (Sj.op_dst e));;
+    let n1 = (int_of_nat (Sj.op_src e)) in
+    let n2 = (int_of_nat (Sj.op_dst e)) in
+    let n1, n2 = match Sj.op_t e with
+    | Sj.FORK -> (n1, n2)
+    | Sj.JOIN -> (n2, n1)
+    in
+    Printf.sprintf "%d -> %d [label=%s]" n1 n2 (string_of_event_t (Sj.op_t e));;
 
 let rec from_list l =
     match l with
@@ -79,27 +107,45 @@ let rec string_of_trace (t:Sj.op Sj.list) :  string =
 
 let string_of_edge e =
     match e with
-    | Sj.Pair (x,y) -> Printf.sprintf "(%d, %d)" (int_of_nat x) (int_of_nat y)
+    | Sj.Pair (x,y) -> Printf.sprintf "%d -> %d [label=KNOWS]" (int_of_nat x) (int_of_nat y)
     ;;    
 
-let rec string_of_graph es =
-    match es with
-    | Sj.Nil -> ""
-    | Sj.Cons (e, es) -> Printf.sprintf "%s, %s" (string_of_edge e) (string_of_graph es)
-    ;;
-
-let x = 1 ;;
-
-let t =
- [{ Sj.op_t=Sj.JOIN; Sj.op_src=Sj.O; Sj.op_dst=(Sj.S Sj.O)};
-  { Sj.op_t=Sj.FORK; Sj.op_src=Sj.O; Sj.op_dst=(Sj.S Sj.O)}];;
+let string_of_graph es =
+    concat "\n" (List.map string_of_edge (filter_duplicates (as_list es)))
 
 let print_is_safe t = match Sj.is_safe t with
-    | Sj.None -> "Unsafe"
+    | Sj.None -> ""
     | Sj.Some es -> string_of_graph es
     ;;
 
-let (>>=) = Lwt.bind
+let to_dot t =
+    let lines = [
+        "digraph G {";
+        print_is_safe t;
+(*        string_of_trace t;*)
+        "}"
+    ] in
+    String.concat "\n" lines
+
+let update_network container s =
+    let convert_dot s =
+        Js.Unsafe.fun_call (Js.Unsafe.js_expr "vis.network.convertDot")
+        [|Js.Unsafe.inject s|] in
+
+    let opts = Js.Unsafe.js_expr "{
+  layout:{
+    randomSeed: 0,
+  },
+  physics: {
+    enabled: true,
+    solver: 'forceAtlas2Based'
+  }
+}
+" in    
+    (* var network = new vis.Network(container, data, options); *)
+    Js.Unsafe.new_obj (Js.Unsafe.variable "vis.Network")
+    [| Js.Unsafe.inject container;  Js.Unsafe.inject (convert_dot s); opts|];
+    Js._false 
 
 let onload _ =
     let txt =
@@ -110,24 +156,17 @@ let onload _ =
         (fun () -> assert false)
         (fun div -> div)
     in
-    let out =
-        Dom_html.getElementById "trace_out"
-        |> Dom_html.CoerceTo.textarea
-        |> fun opt ->
-        Js.Opt.case opt 
-        (fun () -> assert false)
-        (fun div -> div)
-    in
+    let graph =
+        Js.Opt.get (Html.document##getElementById(Js.string "graph_out"))
+        (fun () -> assert false) in
     txt##onkeyup <- Html.handler (
         fun _ ->
         let trace_txt = Js.to_string (txt##value) in
         let t = trace_of_string trace_txt in
-        let result = ["Trace:"; string_of_trace t;
-            "Knowledge graph:"; print_is_safe t] in
-        out##value <- Js.string (String.concat "\n" result);
+        let dot = Js.string (to_dot t) in
+        update_network graph dot;
         Js._false
     );
-    ;
     Js._false
 
 let _ =
